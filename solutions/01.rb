1f65f79060860f8d1908254f8cdab6d592433da4
Reductions <slavi.boyanov@gmail.com>
2016-01-25 20:35:12 +0200

    Solution 1 fix
    Added ' ' after every ',' as specified in the style guide.
    
    Made 'convert_to_bgn' method shorter.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,17 +1,9 @@
-def convert_to_bgn(amount,currency)
-  case currency
-  when :bgn
-    amount.round(2)
-  when :usd
-    (amount * 1.7408).round(2)
-  when :eur
-    (amount * 1.9557).round(2)
-  when :gbp
-    (amount * 2.6415).round(2)
-  end
+def convert_to_bgn(amount, currency)
+  ratio = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
+  (amount * ratio[currency]).round(2)
 end
 
-def compare_prices(first_amount,first_currency,second_amount,second_currency)
-  convert_to_bgn(first_amount,first_currency) <=>
-    convert_to_bgn(second_amount,second_currency)
+def compare_prices(first_amount, first_currency, second_amount, second_currency)
+  convert_to_bgn(first_amount, first_currency) <=>
+    convert_to_bgn(second_amount, second_currency)
 end
