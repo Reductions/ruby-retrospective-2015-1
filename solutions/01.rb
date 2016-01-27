d723d108578609f4903df882c095c5338d347392
Reductions <slavi.boyanov@gmail.com>
2016-01-27 02:26:03 +0200

    Removed unnecessary local variable.
    Removed the ratio local variable from method covert_to_bgn method.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,6 +1,5 @@
 def convert_to_bgn(amount, currency)
-  ratio = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
-  (amount * ratio[currency]).round(2)
+  (amount * {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}[currency]).round(2)
 end
 
 def compare_prices(first_amount, first_currency, second_amount, second_currency)
